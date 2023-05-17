2ddd634093330692e6b3148426015c091575ae44
Gareth Weaver <reg@garethweaver.com>
2023-05-17 11:10:28 +0100

    config(cors): add vercel app

--- api/config.js
+++ api/config.js
@@ -5,6 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   TOKEN: process.env.TOKEN,
   CORS_CONFIG: {
-    origin: ['https://www.garethweaver.com', 'http://localhost:3000'],
+    origin: ['https://www.garethweaver.com', 'http://localhost:3000', 'https://public-site-astro.vercel.app'],
   },
 }
